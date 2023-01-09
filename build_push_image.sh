72d4bb561edc5e348965c5dab0514af649adb6aa
norman <norman.kristiono@gmail.com>
2023-01-09 10:44:37 +0700

    Add update file v2

--- build_push_image.sh
+++ build_push_image.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#variable username untuk login
-uname=krisnn
+uname=krisnn #variable username untuk login
 
-# perintah docker untuk melakukan build image
-docker build --tag "krisnn/item-app:v1" $PWD
+docker build --tag "krisnn/item-app:v1" . # perintah docker untuk melakukan build image
+
+docker images # list semua images yang ada di docker
 
 # perintah docker untuk melakukan login ke docker kemudian status login di masukan ke file process.log
 echo $PASSWORD_DOCKER_HUB | docker login --username=$uname --password-stdin >> process.log
